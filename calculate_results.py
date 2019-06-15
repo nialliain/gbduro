b42ea7bf7cade6ddf32b9105e3ce9ac458e4c48e
nialliain <ESpEuxPocnikKa0>
2019-06-15 06:26:03 +0100

    Initial GBDuro commit

---
+++ calculate_results.py
@@ -0,0 +1,55 @@
+def read_timestamps():
+    import json
+    from datetime import datetime
+    with open( 'timestamps.json' ) as f:
+        results = json.loads(f.read())
+    for rider in results:
+        for point in ['s1-start','s1-end','s2-start','s2-end','s3-start','s3-end','s4-start','s4-end']:
+            if rider[point] != '':
+                rider[point] = datetime.strptime(rider[point], '%Y-%m-%dT%H:%M')
+    return results
+
+def write_results(results, filename):
+    import json
+    with open(filename, 'w') as f:
+        json.dump( {'data': results}, f, indent=2)
+
+def calculate_results(timestamps):
+    from datetime import timedelta
+    last_result_key = None
+    for stage in xrange(1,5):
+        for rider in timestamps:
+            start_key = 's{}-start'.format(stage)
+            end_key = 's{}-end'.format(stage)
+            result_key = 's{}-result'.format(stage)
+            if rider[start_key] and rider[end_key] and (last_result_key is None or last_result_key in rider):
+                rider[result_key] = rider.get(last_result_key, timedelta(0)) + rider[end_key] - rider[start_key]
+        last_result_key = result_key
+    return timestamps
+
+def format_results(results):
+    from collections import OrderedDict
+    formatted_results = []
+    for rider in results:
+        rider_results = OrderedDict()
+        rider_results['Rider'] = rider['displayName']
+        rider_results['CP1'] = format_timedelta(rider.get('s1-result', ''))
+        rider_results['CP2'] = format_timedelta(rider.get('s2-result', ''))
+        rider_results['CP3'] = format_timedelta(rider.get('s3-result', ''))
+        rider_results['CP4'] = format_timedelta(rider.get('s4-result', ''))
+        formatted_results.append( rider_results )
+    return formatted_results
+
+def format_timedelta( td ):
+    from datetime import timedelta
+    if not isinstance(td, timedelta):
+        return td
+    hours = (td.days * 24) + (td.seconds / 3600)
+    minutes = (td.seconds % 3600) / 60
+    return '{:03d}H{:02d}M'.format(hours, minutes)
+
+if __name__ == "__main__":
+    ts = read_timestamps()
+    r = calculate_results(ts)
+    fr = format_results(r)
+    write_results(fr, 'output/19-test.json')
---
+++ test_calculate_results.py
@@ -0,0 +1,101 @@
+from unittest import TestCase
+from datetime import datetime
+from calculate_results import calculate_results, format_results
+
+
+class TestCalculateResults(TestCase):
+
+    def test_calculate_results_midStage(self):
+        ts = [{
+            'displayName': 'Test Rider',
+            's1-start': datetime(2019, 1, 1, 8, 0),
+            's1-end': '',
+            's2-start': '',
+            's2-end': '',
+            's3-start': '',
+            's3-end': '',
+            's4-start': '',
+            's4-end': '',
+        }]
+        results = format_results( calculate_results(ts))
+        self.assertEqual( 'Test Rider', results[0]['Rider'])
+        self.assertEqual( '', results[0]['CP1'])
+        self.assertEqual( '', results[0]['CP2'])
+        self.assertEqual( '', results[0]['CP3'])
+        self.assertEqual( '', results[0]['CP4'])
+
+    def test_calculate_results_afterFirst(self):
+        ts = [{
+            'displayName': 'Test Rider',
+            's1-start': datetime(2019, 1, 1, 8, 0),
+            's1-end': datetime(2019, 1, 2, 23, 59),
+            's2-start': '',
+            's2-end': '',
+            's3-start': '',
+            's3-end': '',
+            's4-start': '',
+            's4-end': '',
+        }]
+        results = format_results( calculate_results(ts))
+        self.assertEqual( 'Test Rider', results[0]['Rider'])
+        self.assertEqual( '039H59M', results[0]['CP1'])
+        self.assertEqual( '', results[0]['CP2'])
+        self.assertEqual( '', results[0]['CP3'])
+        self.assertEqual( '', results[0]['CP4'])
+
+    def test_calculate_results_afterSecond(self):
+        ts = [{
+            'displayName': 'Test Rider',
+            's1-start': datetime(2019, 1, 1, 8, 0),
+            's1-end': datetime(2019, 1, 2, 23, 59),
+            's2-start': datetime(2019, 1, 3, 8, 0),
+            's2-end': datetime(2019, 1, 5, 10, 22),
+            's3-start': '',
+            's3-end': '',
+            's4-start': '',
+            's4-end': '',
+        }]
+        results = format_results( calculate_results(ts))
+        self.assertEqual( 'Test Rider', results[0]['Rider'])
+        self.assertEqual( '039H59M', results[0]['CP1'])
+        self.assertEqual( '090H21M', results[0]['CP2'])
+        self.assertEqual( '', results[0]['CP3'])
+        self.assertEqual( '', results[0]['CP4'])
+
+    def test_calculate_results_afterThird(self):
+        ts = [{
+            'displayName': 'Test Rider',
+            's1-start': datetime(2019, 1, 1, 8, 0),
+            's1-end': datetime(2019, 1, 2, 23, 59),
+            's2-start': datetime(2019, 1, 3, 8, 0),
+            's2-end': datetime(2019, 1, 5, 10, 22),
+            's3-start': datetime(2019, 1, 6, 8, 0),
+            's3-end': datetime(2019, 1, 6, 8, 1),
+            's4-start': '',
+            's4-end': '',
+        }]
+        results = format_results( calculate_results(ts))
+        self.assertEqual( 'Test Rider', results[0]['Rider'])
+        self.assertEqual( '039H59M', results[0]['CP1'])
+        self.assertEqual( '090H21M', results[0]['CP2'])
+        self.assertEqual( '090H22M', results[0]['CP3'])
+        self.assertEqual( '', results[0]['CP4'])
+
+    def test_calculate_results_extraResults(self):
+        ts = [{
+            'displayName': 'Test Rider',
+            's1-start': datetime(2019, 1, 1, 8, 0),
+            's1-end': datetime(2019, 1, 2, 23, 59),
+            's2-start': datetime(2019, 1, 3, 8, 0),
+            's2-end': datetime(2019, 1, 5, 10, 22),
+            's3-start': '',
+            's3-end': '',
+            's4-start': datetime(2019, 1, 6, 8, 0),
+            's4-end': datetime(2019, 1, 6, 8, 1),
+        }]
+        results = format_results( calculate_results(ts))
+        self.assertEqual( 'Test Rider', results[0]['Rider'])
+        self.assertEqual( '039H59M', results[0]['CP1'])
+        self.assertEqual( '090H21M', results[0]['CP2'])
+        self.assertEqual( '', results[0]['CP3'])
+        self.assertEqual( '', results[0]['CP4'])
